059d53648dc52bd8372faa93e4f7489f0ae27324
noa menjikoff <noa.menjikoff@gmail.com>
2024-01-18 16:32:03 +0100

    modif homepage

--- appli/HomePage.java
+++ appli/HomePage.java
@@ -1,21 +1,19 @@
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.StackPane;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.geometry.Insets;
 
-public class HomePage extends GridPane{
+public class HomePage extends BorderPane {
     private AppliReseau appli;
 
-    public HomePage(ConnexionMySQL connexion, AppliReseau appli){
+    public HomePage(ConnexionMySQL connexion, AppliReseau appli) {
         this.appli = appli;
 
-
-        GridPane grid = new GridPane();
-        grid.setPadding(new Insets(60));
+        // Left side with buttons
+        GridPane leftGrid = new GridPane();
+        leftGrid.setPadding(new Insets(60));
 
         Button buttonFollowed = new Button("Accédez à ces follows");
         Button buttonMessage = new Button("Accédez à mes discussions");
@@ -23,25 +21,32 @@ public class HomePage extends GridPane{
         Button button4 = new Button("4");
         Button button5 = new Button("5");
 
-        grid.add(buttonFollowed,0, 0);
-        grid.add(buttonMessage,0,1);
-        grid.add(button3,0, 2);
-        grid.add(button4,0, 3);
-        grid.add(button5,0, 4);
-        
-        ControleurFollow controleurFollow = new ControleurFollow(this.appli, this, connexion);
-        ControleurMessage controleurMessage = new ControleurMessage(this.appli, this, connexion);
-        Controleur3 controleur3 = new Controleur3(appli, this, connexion);
-        Controleur4 controleur4 = new Controleur4(appli, this,connexion);
-        Controleur5 controleur5 = new Controleur5(appli, this, connexion);
+        leftGrid.add(buttonFollowed, 0, 0);
+        leftGrid.add(buttonMessage, 0, 1);
+        leftGrid.add(button3, 0, 2);
+        leftGrid.add(button4, 0, 3);
+        leftGrid.add(button5, 0, 4);
 
-        buttonFollowed.setOnAction(controleurFollow);
-        buttonMessage.setOnAction(controleurMessage);
-        button3.setOnAction(controleur3);
-        button4.setOnAction(controleur4);
-        button5.setOnAction(controleur5);
+        setLeft(leftGrid);
 
-        this.getChildren().addAll(grid);
+        // Center for content (initially empty)
+        GridPane centerGrid = new GridPane();
+        setCenter(centerGrid);
 
+        // Event handling
+        buttonFollowed.setOnAction(e -> handleButtonAction("Button Followed"));
+        buttonMessage.setOnAction(e -> handleButtonAction("Button Message"));
+        button3.setOnAction(e -> handleButtonAction("Button 3"));
+        button4.setOnAction(e -> handleButtonAction("Button 4"));
+        button5.setOnAction(e -> handleButtonAction("Button 5"));
+    }
+
+    private void handleButtonAction(String buttonLabel) {
+        // Replace the content in the center based on the clicked button
+        GridPane newContent = new GridPane();
+        newContent.setPadding(new Insets(10));
+        newContent.add(new Button("New Content for " + buttonLabel), 0, 0);
+
+        setCenter(newContent);
     }
 }
